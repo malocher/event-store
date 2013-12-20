d9db3220bbcce3ca43bc778138cfb8a74720fb39
Manfred Weber <crafics@php.neet>
2013-12-20 11:05:08 +0100

    added exception classes skeleton

---
+++ src/Malocher/EventStore/EventSourcing/EventSourcingException.php
@@ -0,0 +1,24 @@
+<?php
+/*
+ * This file is part of the codeliner/event-store package.
+ * (c) Alexander Miertsch <kontakt@codeliner.ws>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Malocher\EventStore\EventSourcing;
+
+/**
+ * EventSourcingException
+ * 
+ * @author Manfred Weber <crafics@php.net>
+ */
+class EventSourcingException extends \Exception
+{
+    /*
+    public static function exampleException($msg)
+    {
+        return new self('[EventSourcing Error] ' . $msg . "\n");
+    }
+    */
+}
